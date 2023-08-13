4a5a58f2bdb76710ba243c6f0900ee5ec0cfc8fb
OborinaLyubov <Developer.Khudyakova@yandex.ru>
2023-08-14 01:47:49 +0400

    * настройка базовой регистрации * подключение сервиса для отправки уведомлений по почте * добавление констант и обработка ошибок

---
+++ TravelPlanerLib/Models/RegisterViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TravelPlanerLib.Models
+{
+    /// <summary>
+    /// Модель для хранения данных по регистрируемому пользователю
+    /// </summary>
+    public class RegisterViewModel
+    {
+        /// <summary>
+        /// ФИО пользователя
+        /// </summary>
+        [Required]
+        [Display(Name = "ФИО пользователя")]
+        public string Name { get; set; }
+        /// <summary>
+        /// Логин
+        /// </summary>
+        [Required]
+        [Display(Name = "Логин")]
+        public string Login { get; set; }
+        /// <summary>
+        /// Email
+        /// </summary>
+        [Required]
+        [Display(Name = "Email пользователя")]
+        public string Email { get; set; }
+        /// <summary>
+        /// Пароль
+        /// </summary>
+        [Required]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+        /// <summary>
+        /// Повторный ввод пароля
+        /// </summary>
+        [Required]
+        [Display(Name = "Повторите пароль")]
+        public string PasswordConfirm { get; set; }
+    }
+}
